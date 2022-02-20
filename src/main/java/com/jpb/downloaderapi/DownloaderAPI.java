ba88b7f78a24cf59a73658b68a9eb33d9e483829
jpb <jakub.baranski.33@gmail.com>
2022-02-20 19:26:27 +0000

    version 1.1.0 Optimization: Removes ALL external dependencies (except androidx.core) Feature: Adds ability to save in DCIM and Documents directories as well Feature: Adds ability to show download progress of file and total file size

--- src/main/java/com/jpb/downloaderapi/DownloaderAPI.java
+++ src/main/java/com/jpb/downloaderapi/DownloaderAPI.java
@@ -3,18 +3,41 @@ package com.jpb.downloaderapi;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+import android.widget.Toast;
+
+import java.lang.Exception;
 
 public class DownloaderAPI {
-    public static void download(Context context, String uri, String filetitle, String filename) {
+    public static void download(Context context, String uri, String filetitle, String filename, Boolean downloadingTextIsNew, Integer SaveDirectory) {
         DownloadManager downloadmanager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
         Uri urifunction = Uri.parse(uri);
 
         DownloadManager.Request request = new DownloadManager.Request(urifunction);
         request.setTitle(filetitle);
-        request.setDescription("Downloading");
+        if (!downloadingTextIsNew) {
+            request.setDescription("Downloading");
+        } else {
+            request.setDescription("Downloading... " + DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR + " / " + DownloadManager.COLUMN_TOTAL_SIZE_BYTES);
+        }
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename);
+        if (SaveDirectory == 1) {
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename);
+        } else if (SaveDirectory == 2) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOCUMENTS, filename);
+            } else {
+                Toast.makeText(context.getApplicationContext(),
+                        "Your device is running Android version below 4.4 (KitKat), so your file will be saved to Downloads folder instead of Documents folder.",
+                        Toast.LENGTH_LONG).show();
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename);
+            }
+        } else if (SaveDirectory == 3) {
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DCIM, filename);
+        } else {
+            SaveDirectory = 0;
+        }
         downloadmanager.enqueue(request);
     }
 }
