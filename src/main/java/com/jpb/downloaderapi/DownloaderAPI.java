86eafb94611550c8180a311ecefcb23363009e0f
jpb <jakub.baranski.33@gmail.com>
2022-02-20 10:19:29 +0000

    Initial commit

---
+++ src/main/java/com/jpb/downloaderapi/DownloaderAPI.java
@@ -0,0 +1,20 @@
+package com.jpb.downloaderapi;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
+
+public class DownloaderAPI {
+    public void download(Context context, String uri, String filetitle, String filename) {
+        DownloadManager downloadmanager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri urifunction = Uri.parse(uri);
+
+        DownloadManager.Request request = new DownloadManager.Request(urifunction);
+        request.setTitle(filetitle);
+        request.setDescription("Downloading");
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename);
+        downloadmanager.enqueue(request);
+    }
+}
